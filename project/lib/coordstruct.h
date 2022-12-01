f7fc0a103ada9b98e5800da14606a8d7dc79ca4a
kamamesa <katla.maria.meyer@nmbu.no>
2022-12-01 14:17:12 +0100

    img_gen has segmentation fault and edge detection is pseudocode, but it's fiiine

--- project/lib/coordstruct.h
+++ project/lib/coordstruct.h
@@ -5,6 +5,7 @@ struct coords{
 	int x;
 	int y;
 
+	// Little tool to check whether I exist
 	bool operator==(coords const &a){
 		return (this->x == a.x) && (this->y == a.y);
 	}
--- project/lib/edge_detection.cpp
+++ project/lib/edge_detection.cpp
@@ -2,9 +2,9 @@
 
 // create array/string/whatever
 
-// read bpm file
+// read pbm array, ie skip P6 [size] and then start reading everything except spaces I guess?
 
-// go through points and apply magic ?
+// go through points and apply magic
 
 // if a point matches the magic, add to array
 
