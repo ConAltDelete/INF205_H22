07f22f580bd9dbb270e82d360f53fcb0732d31c0
Mats Hoem Olsen <mats.hoem.olsen@nmbu.no>
2022-12-01 21:13:16 +0100

    made an implementation of edge detection

--- project/lib/coordstruct.h
+++ project/lib/coordstruct.h
@@ -1,6 +1,10 @@
 #ifndef HEADER_GUARD
 #define HEADER_GUARD
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 struct coords{
 	int x;
 	int y;
@@ -9,6 +13,16 @@ struct coords{
 	bool operator==(coords const &a){
 		return (this->x == a.x) && (this->y == a.y);
 	}
-}
+};
+
+/*
+ *	gets the next clockwise diraction based on two points A, and B.
+ * */
+coords get_next_dir(coords A, coords B);
+
+/*
+ * traverses through the image until it hits a pixel not equal to the background. Then it looks clockwise/anti-clockwise from the previus pixle in the Moore Neighborhood until it finds a new point, and then creates a new Moore neighborhood. This continues until it findes the original point.
+ */
+std::vector<coords> contour(std::vector<coords> image, coords start_point, coords pre_point);
 
 #endif
