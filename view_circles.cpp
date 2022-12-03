4d02c6bc8dcfb99a76ade354c66c198fcd4f3527
Oda <oda.johanne.matre.simensen@nmbu.no>
2022-12-03 13:36:57 +0100

    Laget en fil form mine tidligere (feilende) koder

--- view_circles.cpp
+++ view_circles.cpp
@@ -4,10 +4,7 @@
 
 
 int x, y, r;
-//Just for testing purpouses
-x = 5;
-y = 3;
-r = 70;
+
 
 // standard text for all circles
 string ps_circle;
@@ -16,12 +13,9 @@ ps_circle = "0 360 arc fill";
 // making a file for the PostScript code
 ofstream fcircle("circles.txt");
 
-for (int i = 0; i < 5; i++) {
-    ++x, ++y, ++r;
-    fcircle <<x<<" "<<y<<" "<<r<<" "<<ps_circle<<"\n";
+for (int obj : obj_lst) {
+    fcircle <<obj.x<<" "<<obj.y<<" "<<obj.r<<" "<<ps_circle<<"\n";
 }
 
 fcircle.close();
 
-// Just for confirmation atm
-cout<<x<<" "<<y<<" "<<r<<" "<<ps_circle <<endl;
